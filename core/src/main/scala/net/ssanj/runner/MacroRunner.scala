c936798100bf7ab350b8f7c2ba6aeb3cc50c2736
sanjiv sahayam <sanjsmailbox@gmail.com>
2018-04-23 23:02:01 +1000

    refactor to zen

--- core/src/main/scala/net/ssanj/runner/MacroRunner.scala
+++ core/src/main/scala/net/ssanj/runner/MacroRunner.scala
@@ -4,60 +4,37 @@ import monix.execution.Scheduler.Implicits.global
 import monix.reactive._
 import concurrent.duration._
 
-import net.ssanj.inspect._
-
 object MacroRunner {
   def main(args: Array[String]): Unit = {
-    // showT(List(1,2,3,4))
-    // showT(List(1,2,3,4).map(_.toString).mkString)
-    val source = inspect(Observable.interval(1.second)
-      // Filtering out odd numbers, making it emit every 2 seconds
+    zen.t(List(1,2,3,4))
+    zen.t(List(1,2,3,4).map(_.toString).mkString)
+
+    val source = zen.inspect(Observable.interval(1.second)
       .filter(_ % 2 == 0)
-      // We then make it emit the same element twice
       .flatMap(x => Observable(x, x)))
-      // This stream would be infinite, so we limit it to 10 items
       . take(11)
 
-    val cancelable = showA(source
-      // On consuming it, we want to dump the contents to stdout
-      // for debugging purposes
+    val cancelable = zen.ast(source
       .dump("O"))
-      // Finally, start consuming it
       .subscribe()
 
-    val l: Either[String, Int] = Left("error")
-    showT(l)
-
     val x =
-      explain(for {
+      zen.explain(for {
         one <- Option(1)
         two <- Option(2)
         if (one > 5)
       } yield (one + two))
 
 
-    explain{ val (even, odd) = List(1,2,3,4).partition(_ % 2 == 0) }
-
-    explain{ val x = List(1,2,3,4).partition(_ % 2 == 0) }
-
-    explain { Option(1) match {
-        case Some(x) if x == 1 => true
-      }
-    }
-
-    explain { Option(1) match {
+    zen.explain { Option(1) match {
         case Some(x) => true
         case None => false
       }
     }
 
-    inspect {
-      List(1,2,3,4).map(x => if (x % 3 != 0) Option(x) else None).collect { case Some(x) =>  x}
-    }
-
     Observable.range(0, 4).dump("O")
       .consumeWith(Consumer.complete)
       .runAsync
-      .foreach(x => { showTN("x", x); println("Consumer completed") })
+      .foreach(x => { zen.t("x", x); println("Consumer completed") })
   }
 }
---
+++ macro/src/main/scala/package.scala
@@ -0,0 +1,66 @@
+import scala.reflect.macros.blackbox.Context
+import scala.language.experimental.macros
+import scala.io.AnsiColor._
+
+package object zen {
+
+  sealed trait MacroType
+  case object Type extends MacroType
+  final case class NamedType(name: String) extends MacroType
+  case object AST extends MacroType
+  case object Explain extends MacroType
+
+  def t[A](value: A): A = macro typeMacro[A]
+
+  def t[A](name: String, value: A): A = macro namedTypeMacro[A]
+
+  def explain[A](value: A): A = macro explainMacro[A]
+
+  def ast[A](value: A): A = macro astMacro[A]
+
+  def inspect[A](value: A): A = macro inspectMacro[A]
+
+  def typeMacro[A: c.WeakTypeTag](c: Context)(value: c.Expr[A]): c.Expr[A] = {
+    val tree = c.typecheck(value.tree)
+    console(Type, tree.tpe.toString )
+    value
+  }
+
+  def namedTypeMacro[A: c.WeakTypeTag](c: Context)(name: c.Expr[String], value: c.Expr[A]): c.Expr[A] = {
+    val tree = c.typecheck(value.tree)
+    console(NamedType(name.tree.toString), tree.tpe.toString )
+    value
+  }
+
+  def explainMacro[A: c.WeakTypeTag](c: Context)(value: c.Expr[A]): c.Expr[A] = {
+    import c.universe._
+
+    console(Explain, showCode(value.tree))
+    value
+  }
+
+  def astMacro[A: c.WeakTypeTag](c: Context)(value: c.Expr[A]): c.Expr[A] = {
+    import c.universe._
+
+      console(AST, showRaw(value.tree))
+    value
+  }
+
+  def inspectMacro[A: c.WeakTypeTag](c: Context)(value: c.Expr[A]): c.Expr[A] = {
+    explainMacro[A](c)(value)
+    astMacro[A](c)(value)
+    typeMacro[A](c)(value)
+    value
+  }
+
+  private def console(macroType: MacroType, value: String): Unit = {
+    val (prefix, colour) = macroType match {
+      case Type            => ("type", GREEN)
+      case NamedType(name) => (s"type $name", GREEN)
+      case AST             => ("ast", CYAN)
+      case Explain         => ("code", YELLOW)
+    }
+
+    println(s"${colour}${prefix}${RESET}: ${value}")
+  }
+}
